0f3d0aa163e7764d7c1455ceb6b5c0f6ff76416b
marcelo <s.framinet@gmail.com>
2015-01-20 16:43:56 +0100

    Création et implémentation de la classe DbConnection.

--- projet-bd/src/BD/DbConnection.java
+++ projet-bd/src/BD/DbConnection.java
@@ -1,5 +1,27 @@
 package BD;
 
-public class DbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+
+//classe qui permet de gérer la connection à la base de donnée
+public class DbConnection {
+	
+	private static Connection instanceBD = null;
+	private static String url = "jdbc:oracle:thin:@im2ag-oracle.e.ujf-grenoble.fr:1521:ufrima";
+	private static String user = "framines";
+	private static String passwd = "bd2015";
+	
+	  public static Connection getInstance() {      
+		  try {
+	    	if(instanceBD==null){
+	    		Connection conn = DriverManager.getConnection(url, user, passwd);
+	    		System.out.println("--Connexion à la base de donnée effectué--");  
+	    	}
+	         
+		  }catch (Exception e) {
+	      e.printStackTrace();
+	    }     
+	    return instanceBD;
+	  }
 }
