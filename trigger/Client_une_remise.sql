acaaf9bd02bddd0dffdd7136a88bed883c881ce5
marcelo <s.framinet@gmail.com>
2015-01-20 16:06:21 +0100

    Initialisation du dépot + création du premier Trigger

--- trigger/Client_une_remise.sql
+++ trigger/Client_une_remise.sql
@@ -1,1 +1,16 @@
+
+/*Un client ne peut pas avoir plus d’une remise*/
+
 CREATE OR REPLACE TRIGGER Client_une_remise
+Before create on remise
+
+begin
+
+/*on regarde si le client a déja une remise*/
+
+Select id_remise
+From remise
+Where :new
+
+end;
+/
