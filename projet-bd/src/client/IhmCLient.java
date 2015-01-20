2a4c5ddf3dc41ea1af8d2c9e58ebe6d069785a9f
marcelo <s.framinet@gmail.com>
2015-01-20 16:59:13 +0100

    Création de la classe ihmClient + Commencement de l'implémentation

---
+++ projet-bd/src/client/IhmCLient.java
@@ -0,0 +1,22 @@
+package client;
+
+//Class qui gère l'ihm du client 
+public class IhmCLient {
+
+	public static void main(String[] args) {
+		
+		System.out.println("----------------------------------------------------");
+		System.out.println("Bienvenue dans l'application dédié au client");
+		System.out.println("----------------------------------------------------\n");
+		
+		
+		System.out.println("Menu : ");
+		System.out.println("1) Louer un Vélo");
+		System.out.println("2) S'abonner");
+	}
+	
+	
+	
+
+
+}
